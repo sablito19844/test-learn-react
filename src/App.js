614bcaf2dc459970c6c224777f0cc0d9ea9eedf1
Amer <atoumi@externe.europ-assistance.com>
2022-06-23 18:50:02 +0200

    setup app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
 import React, { Component } from "react";
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-import Home from './Component/Home'
-import About from './Component/About'
-import Nav from './Component/Nav'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Nav from "./Component/nav";
 
 class App extends Component {
   render() {
     return (
       <div>
-      <Nav />
-      < BrowserRouter >
-        <Routes>
-
-          <Route path="/home" component={Home} />
-          <Route path="/about" component={About} />
-
-          </ Routes>
-          </ BrowserRouter >
-          </div>
-          ) 
+        <BrowserRouter>
+        <Nav />
+          <Routes>
+            <Route path="home" element={<Home />} />
+            <Route path="about" element={<About />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    );
   }
 }
-export default App
+export default App;
