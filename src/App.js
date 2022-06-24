dc1ac37c8c10fbd124b6995d6b5654c729caed05
Amer <atoumi@externe.europ-assistance.com>
2022-06-24 12:27:28 +0200

    Add components folder

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Nav from "./Component/nav";
+import Nav from "./Components/nav";
 
 class App extends Component {
   render() {
---
+++ src/Components/nav.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./nav.css";
+const Nav = () => {
+  return (
+    // test
+    <div className="navbar">
+      <div className="container">
+        <a href="##" className="logo">
+          Logo
+        </a>
+        <ul>
+          <a href="/home">Home</a>
+          <a href="/about">About</a>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
